eeb58be20e23365ced883a221e876f879df8cd34
Jyanti Kumar <jyantiaustriakumar@gmail.com>
2025-12-08 15:44:33 +0800

    3

--- src/assets/Portfolio.jsx
+++ src/assets/Portfolio.jsx
@@ -96,7 +96,7 @@ const Portfolio = () => {
         </div>
         
          {/* Project Grid */}
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-10">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
           {projectData.map((project) => (
             <div 
               key={project.id}
